0c78d57fbfc24122a0eee2213c2cf2c79d691600
VitaliiBaryliuk <VitaliiBaryliuk@gmail.com>
2025-09-18 22:25:13 +0300

    fix: correct tests

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,10 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World 1!';
+    return 'Hello World!';
   }
 
   getHola(): string {
-    return 'Hola Hola Hola World!';
+    return 'Hola World!';
   }
 }
